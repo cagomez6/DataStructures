05fabec85d7b2a821d39ab0a853d24f3488a7086
cagomez6 <64633124+cagomez6@users.noreply.github.com>
2023-01-20 19:27:20 -0800

    Create doubly.js

---
+++ linkedList/doubly.js
@@ -0,0 +1,121 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+        this.prev = null;
+    }
+}
+
+class LinkedList {
+    constructor(value) {
+        this.head = new Node(value);
+        this.tail = this.head;
+        this.length = 1;
+    }
+
+    append(value) {
+        const newNode = new Node(value);
+        newNode.prev = this.tail;
+        this.tail.next = newNode;
+        this.tail = newNode;
+        this.length++;
+    }
+
+    prepend(value) {
+        const newNode = new Node(value);
+        newNode.next = this.head;
+        this.head.prev = newNode;
+        this.head = newNode;
+        this.length++;
+    }
+
+    insert(index, value) {
+        //check valid index
+        if(index === 0) {
+            return this.prepend(value);
+        }
+
+        if(index === this.length) {
+            return this.append(value);
+        }
+
+        if(index > 0 && index < this.length)
+        {
+            const newNode = new Node(value);
+
+            let current = this._iterateTo(index);
+
+            newNode.next = current.next;
+            newNode.prev = current;
+            current.next = newNode;
+
+            if(newNode.next) {
+                let nextNode = newNode.next;
+                nextNode.prev = newNode;
+            }
+
+            this.length++;
+        }
+    }
+
+    remove(index) {
+        //check valid index
+        if(index >= 0 && index < this.length)
+        {
+            //if first item, set new head
+            if(index === 0) {
+                let temp = this.head;
+                this.head = temp.next;
+                this.head.prev = null;
+                temp = null;
+            } else {
+                let current = this._iterateTo(index);
+                let deleteTarget = current.next;
+                current.next = deleteTarget.next;
+
+                if(deleteTarget.next) {
+                    let nextNode = deleteTarget.next;
+                    nextNode.prev = current;
+                }
+
+                deleteTarget.next = null;
+                deleteTarget.prev = null;
+                deleteTarget = null;
+
+                //if last item, set new tail
+                if(index === this.length-1) {
+                    this.tail = current;
+                }
+            }
+            this.length--;
+        }
+    }
+
+    _iterateTo(index) {
+        let current = this.head;
+        for(let i = 0; i < index-1; i++)
+        {
+            current = current.next;
+        }
+        return current;
+    }
+
+    printList() {
+        const array = [];
+        let current = this.head;
+        while(current !== null) {
+            array.push(current.value);
+            current = current.next;
+        }
+        return array;
+    }
+}
+
+const myLinkedList = new LinkedList(10);
+myLinkedList.append(5);
+myLinkedList.append(16);
+myLinkedList.prepend(1);
+myLinkedList.insert(2,7);
+myLinkedList.remove(4);
+console.log(myLinkedList);
+console.log(myLinkedList.printList());
